9aaafa21fc2acf449f9924fc05ef49ec9c0919e9
oasis.szli <oasis.szli@68123ae8-0f5e-af55-4811-21dd76c9e64b>
2010-04-16 08:52:09 +0000

    init

---
+++ trunk/server/op/views.py
@@ -0,0 +1,171 @@
+# # -*- coding: utf-8 -*-  
+from django.http import HttpResponse
+import smth.util.mcconns as context
+from smth.util.smthapi import SimpleSMTH
+from smth.util.smthparser import FavProcessor
+from smth.util.smthparser import BoardProcessor
+from smth.util.smthparser import ArticleProcessor
+from smth.util.json import json_encode
+from smth.util.json import json_result
+from django.conf import settings
+import logging
+import os
+
+smthurl=settings.SMTH_URL
+
+def login(request):
+    userid=request.GET['id']
+    passwd=request.GET['passwd']
+    smth2=SimpleSMTH()
+    r=smth2.smthLogin(userid,passwd)
+    logging.info('login user:'+userid+',r:'+str(r))
+    if r == 0:
+        key=context.gen_sid()
+        smth2.saveCj(key)
+    else:
+        key='0' #login fail
+    result = {}
+    result['k'] = key
+    return HttpResponse(json_encode(result))
+
+def logout(request):
+    try:
+        key=request.GET['key']
+    except:
+        logging.error('login key not found ')
+        fail = {}
+        fail['l']=0
+        return HttpResponse(json_encode(fail))    
+    result={}
+    result['l']=1
+    try:
+        context.remove_ck(key)
+        result['r'] = 0
+    except:
+        logging.warn('logout a invalid key')
+        result['r'] = 1
+    logging.info('logout key:'+key)
+    result2 = json_encode(result)
+    return HttpResponse(result2)
+            
+    
+def getfav(request):
+    try:
+        key=request.GET['key']
+#        cjpath = context.getConnection(key)
+        smth2 = SimpleSMTH()
+        smth2.setCj(key)
+    except:
+        logging.error('login key not found ')
+        fail = {}
+        fail['l']=0
+        return HttpResponse(json_encode(fail))
+    result={}
+    result['l']=1
+    simpleout=smth2.get_url_data(smthurl)
+    print simpleout
+    process = FavProcessor()
+    process.feed(simpleout)
+    result['r']=process.getfav()
+    result2 = json_encode(result)
+    process.close
+    logging.info('getfav,r:'+result2)
+    print result2
+    return HttpResponse(result2)
+    
+    
+def getboard(request):
+    try:
+        key=request.GET['key']
+        smth2 = SimpleSMTH()
+        smth2.setCj(key)
+    except:
+        fail = {}
+        fail['l']=0
+        return HttpResponse(json_encode(fail))
+    result={}
+    result['l']=1
+    name = request.GET['board']
+    try:
+        page = request.GET['page']
+    except:
+        page = 0
+    try:
+        ftype = request.GET['ftype']
+    except:
+        ftype = 0
+    if page > 0:
+        simpleout=smth2.get_url_data(smthurl+'?act=board&board='+name+'&page='+page+'&ftype='+str(ftype))
+    else:
+        simpleout=smth2.get_url_data(smthurl+'?act=board&board='+name+'&ftype='+str(ftype))
+    process = BoardProcessor()
+    process.feed(unicode(simpleout,"gbk"))
+#    process.feed(simpleout)
+    result['r']=process.showboard()
+    result['pp']=process.getppage()
+    result['np']=process.getnpage()
+    process.close
+    result2 = json_encode(result)
+    print result2
+    return HttpResponse(result2)
+     
+def article(request):
+    try:
+        key=request.GET['key']
+        smth2 = SimpleSMTH()
+        smth2.setCj(key)
+    except:
+        fail = {}
+        fail['l']=0
+        return HttpResponse(json_encode(fail))
+    result={}
+    result['l']=1
+    articleId = request.GET['id']
+    board = request.GET['board']
+    try:
+        p = request.GET['p']
+    except:
+        p = 0
+    if p!=0:
+        simpleout=smth2.get_url_data(smthurl+'?act=article&board='+board+'&id='+articleId+'&p='+p)
+    else:
+        simpleout=smth2.get_url_data(smthurl+'?act=article&board='+board+'&id='+articleId)
+    process = ArticleProcessor()
+    process.feed(unicode(simpleout,"gbk"))
+#    process.feed(simpleout)
+    content = process.show()
+    result['r']=content
+    result['t']=process.gettitle()
+    result['tid'] = process.gettopid()
+    result['id']=process.getid()
+    process.close
+    result2 = json_encode(result)
+    print result2
+    return HttpResponse(result2)
+
+def post(request):
+    try:
+        key=request.GET['key']
+        smth2 = SimpleSMTH()
+        smth2.setCj(key)
+    except:
+        fail = {}
+        fail['l']=0
+        return HttpResponse(json_encode(fail))
+    result={}
+    result['l']=1
+    board= request.GET['board']
+    title = request.POST['title']
+    content = request.POST['content']
+    reid = request.GET['reid']
+    data = {'title':title.encode('gbk'),'text':content.encode('gbk')}
+    print data
+    simpleout=smth2.post_url_data(smthurl+'?act=post&board='+board+'&reid='+reid+'&post=1',data)
+    if len(simpleout) > 1:
+        result['r'] = 0
+    else:
+        result['r'] = 1
+    result2 = json_encode(result)
+    print result2
+    return HttpResponse(result2)
+
---
+++ trunk/server/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^smth/', include('smth.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/', include(admin.site.urls)),
+    (r'^login', 'smth.op.views.login'),
+    (r'^logout', 'smth.op.views.logout'),
+    (r'^getfav', 'smth.op.views.getfav'),
+    (r'^getboard', 'smth.op.views.getboard'),
+    (r'^article', 'smth.op.views.article'),
+    (r'^post', 'smth.op.views.post'),
+)
